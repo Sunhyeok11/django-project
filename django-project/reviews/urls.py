d3ccb2f2e7639175eb4138cac483b3a12c1f604f
SunHyeok <cghhld@naver.com>
2023-04-13 16:24:44 +0900

    asw

--- django-project/reviews/urls.py
+++ django-project/reviews/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='index'),
     path('<int:review_pk>/', views.detail, name='detail'),
+    path('<int:artilce_pk>/delete/', views.delete, name='delete'),
+    path('<int:article_pk>/update/', views.update, name='update'),
     path('<int:review_pk>/comments/',views.comment_create, name='comment_create'),
     path('<int:review_pk>/comments/<int:comment_pk>/delete/', views.comments_delete,name='comments_delete'),
 ]
