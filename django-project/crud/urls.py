f1e64e6e1a4d5e25d0aa5879fdfdf05ed124e0d9
illson97 <illson97@github.com>
2023-04-13 16:16:41 +0900

    push

--- django-project/crud/urls.py
+++ django-project/crud/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', include('reviews.url')),
+    path('reviews/', include('reviews.urls')),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
