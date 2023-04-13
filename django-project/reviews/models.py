3cdd94e2aef9e512dca16fd7bd49b371aac9f8eb
SunHyeok <cghhld@naver.com>
2023-04-13 14:47:16 +0900

    modela

--- django-project/reviews/models.py
+++ django-project/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField(null=False)
+    movie = models.CharField(max_length=80)
+    image = models.ImageField(blank=True, upload_to='')
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=80)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
